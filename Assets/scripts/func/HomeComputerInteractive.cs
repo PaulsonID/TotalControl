c40d4d767eecff99146a18b87ae9651fe7d425f9
AlexLuzifer <AlexLuzifer@Yandex.ru>
2023-05-12 23:06:59 +0300

    Test

--- Assets/scripts/func/HomeComputerInteractive.cs
+++ Assets/scripts/func/HomeComputerInteractive.cs
@@ -5,64 +5,68 @@ using UnityEngine.UI;
 using TMPro;
 
 public class HomeComputerInteractive : InteractStatus
-{	
-	[SerializeField] private bool interacted = false;
-	
-	[SerializeField] private TMP_InputField terminalInputField;
-	
-	public GameObject text;
-	
-	public string inputValue;
-	
-	// TMP_Text terminalVisualizationInput;
-	
-	
+{
+    [SerializeField] private bool interacted = false;
+
+    [SerializeField] private TMP_InputField terminalInputField;
+
+    public GameObject text;
+
+    public string inputValue;
+
+    // Hello World
+
+    // TMP_Text terminalVisualizationInput;
+
+
     private void Update()
     {
-		if(GetStatus()) StartWorkToPC();
-		
-	/*
-		if (GetStatus()){
-			
-			if (Input.GetKey(KeyCode.Escape)){
-				SetStatus(false);
-			}
-			
-			if (Input.GetKey(KeyCode.Return)){
-			input_text=text1.GetComponent<TMP_Text>().text;
-			}
-		}
-		else if (!GetStatus()){
-			canvas.GetComponent<TMP_InputField>().ActivateInputField();
-			text.GetComponent<TextMesh>().text+=">>Ghostmaker:Привет \n";   
-		  }
-		  
-		  */
+        if (GetStatus()) StartWorkToPC();
+
+        /*
+            if (GetStatus()){
+
+                if (Input.GetKey(KeyCode.Escape)){
+                    SetStatus(false);
+                }
+
+                if (Input.GetKey(KeyCode.Return)){
+                input_text=text1.GetComponent<TMP_Text>().text;
+                }
+            }
+            else if (!GetStatus()){
+                canvas.GetComponent<TMP_InputField>().ActivateInputField();
+                text.GetComponent<TextMesh>().text+=">>Ghostmaker:Привет \n";   
+              }
+
+              */
     }
-	
-	private void StartWorkToPC(){
-		// Приветсвие - once
-		
-		// Режим ввода
-		if (!interacted)terminalInputField.ActivateInputField();
-		interacted=true;
-		if (Input.GetKeyDown(KeyCode.Escape)){
-			//terminalInputField.DeactivateInputField();
-			SetStatus(false);
-			interacted=false;	
-		}
-		//else if(Input.GetMouseButtonDown(1)){
-		//	terminalInputField.DeactivateInputField();
-		//}
-		
-		else if (Input.GetKeyDown(KeyCode.Return)){
-		//	inputValue = terminalVisualizationInput.text;
-		}
-		
-		Debug.Log(terminalInputField.isFocused);
-		// Режим ожидание ответа
-	}
-	
-	
-	
+
+    private void StartWorkToPC()
+    {
+        // Приветствие - once
+
+        // Режим ввода
+        if (!interacted) terminalInputField.ActivateInputField();
+        interacted = true;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //terminalInputField.DeactivateInputField();
+            SetStatus(false);
+            interacted = false;
+        }
+        //else if(Input.GetMouseButtonDown(1)){
+        //	terminalInputField.DeactivateInputField();
+        //}
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //	inputValue = terminalVisualizationInput.text;
+        }
+        // Режим ожидание ответа
+    }
+
+
+
 }
